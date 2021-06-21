8fb9b9b1684a1dc9f39c3a3e8adceb4a63fa7698
ottomattas <otto.mattas@gmail.com>
2021-06-21 21:19:47 +0200

    Add runtime scripts

---
+++ Procedural-Island-Generation/Assets/Scripts/Runtime/RendererProperties.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+/// <summary>
+/// Serializable Renderer properties
+/// </summary>
+[Serializable]
+public class RendererProperties
+{
+    public LightProbeUsage lightProbeUsage = LightProbeUsage.BlendProbes;
+    public GameObject lightProbeProxyVolumeOverride = null;
+    public ReflectionProbeUsage reflectionProbeUsage = ReflectionProbeUsage.BlendProbes;
+    public Transform probeAnchor = null;
+    public ShadowCastingMode shadowCastingMode = ShadowCastingMode.On;
+    public bool receiveShadows = true;
+    public MotionVectorGenerationMode motionVectorGenerationMode = MotionVectorGenerationMode.Object;
+}
