8b4c16691201112c81caf5de0e511755535d35b1
ottomattas <otto.mattas@gmail.com>
2021-06-21 21:20:11 +0200

    Add tools

---
+++ Procedural-Island-Generation/Assets/Scripts/Tools/GroundSkyBoxGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GroundSkyBoxGenerator : ConfiguratorBase
+{
+    private const float generateInterval = 5;
+
+    private float nextGenerateTime;
+
+    private void Awake()
+    {
+        GeneratePalette();
+        SetupSkyboxAndPalette();
+    }
+
+    private void Update()
+    {
+        if (Time.time > nextGenerateTime)
+        {
+            nextGenerateTime = Time.time + generateInterval;
+            GeneratePalette();
+        }
+
+        UpdateSkybox();
+    }
+}
